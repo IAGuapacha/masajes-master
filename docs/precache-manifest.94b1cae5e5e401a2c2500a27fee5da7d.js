0fe63950f4b6ea9895cba869624f2295350856b6
Ivan <iandresguapacha@gmail.com>
2020-11-26 02:15:10 -0600

    docs agregado

---
+++ docs/precache-manifest.94b1cae5e5e401a2c2500a27fee5da7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9516f5785c5035b7e3ff64ee262a3f64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63cf5c5f3b4a0e6604d",
+    "url": "/static/css/main.7b996947.chunk.css"
+  },
+  {
+    "revision": "9fd4004182c9daa5096e",
+    "url": "/static/js/2.09b3e4ae.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.09b3e4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63cf5c5f3b4a0e6604d",
+    "url": "/static/js/main.fafebad6.chunk.js"
+  },
+  {
+    "revision": "fdc8c4dd1c10925dc408",
+    "url": "/static/js/runtime-main.a98aceb4.js"
+  }
+]);
